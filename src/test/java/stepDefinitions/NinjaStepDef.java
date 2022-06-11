9c40ef3f6fa8ead888902bfc427045e654058c48
makifoeren <makif.oeren@gmail.com>
2022-06-11 17:31:30 +0200

    11.06

--- src/test/java/stepDefinitions/NinjaStepDef.java
+++ src/test/java/stepDefinitions/NinjaStepDef.java
@@ -20,6 +20,14 @@ public class NinjaStepDef {
     ~ click on black total added cart button
     ~ get the names of list from the cart
     ~ compare the names from displaying list and cart list
+
+    Navigieren Sie zu http://tutorialsninja.com/demo/index.php?route=common/home
+    ~ Klicken Sie auf Telefone & PDAs
+    ~ Holen Sie sich den Markennamen von Telefonen
+    ~ Klicken Sie für alle Elemente auf die Schaltfläche Hinzufügen
+    ~ Klicken Sie auf die schwarze Schaltfläche zum Hinzufügen des Einkaufswagens
+    ~ Holen Sie sich die Namen der Liste aus dem Warenkorb
+    ~ Vergleichen Sie die Namen aus der Anzeigeliste und der Warenkorbliste
 */
 
 
